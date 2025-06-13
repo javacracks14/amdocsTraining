b027be52e29abc04e074daab35c4c7ff4c60d948
javacracks14 <57252404+javacracks14@users.noreply.github.com>
2025-06-13 10:48:54 +0530

    Add files via upload

---
+++ FirstJavaApplication.java
@@ -0,0 +1,8 @@
+public class FirstJavaApplication
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Java Developers");
+		System.out.println("Bye Java Developers");
+	}
+}
